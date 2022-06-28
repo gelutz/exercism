be2302b9f3a8a07b72d3518eacdeef76a9bc2706
lutz <gabriel.lutz5@gmail.com>
2022-06-28 06:30:58 -0300

    29/06/2022

---
+++ typescript/binary-search/binary-search.ts
@@ -0,0 +1,3 @@
+export function find<T = number>(haystack: T[], needle: T): number {
+	const middle = Math.floor(haystack.length / 2).toFixed(0)
+}
--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,30 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const orbitalPeriods: {[x: string]: number} = {
+	'mercury': 0.2408467,
+	'venus': 0.61519726,
+	'earth': 1,
+	'mars': 1.8808158,
+	'jupiter': 11.862615,
+	'saturn': 29.447498,
+	'uranus': 84.016846,
+	'neptune': 164.79132
 }
+
+const secondsToEartYear = (seconds: number) => (seconds / 31557600)
+
+export const age = (planet: string, seconds: number): number => {
+	const earthAge = secondsToEartYear(seconds)
+
+	if (!Object.keys(orbitalPeriods).includes(planet)) {
+		throw Error('Planet not found.')
+	}
+
+	return +(earthAge / orbitalPeriods[planet]).toFixed(2)
+}
+
+
+
+
+
+
+
+
